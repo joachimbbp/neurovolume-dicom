35eec62e37168a9407f8cf56b81b64a7c130f195
Joachim Pfefferkorn <jbbpfefferkorn@gmail.com>
2026-09-10 17:38:30 -0400

    static volume saves to vdb

--- dicom_to_numpy.py
+++ dicom_to_numpy.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import pydicom
 import numpy as np
-import matplotlib.pyplot as plt
 # load the DICOM files
 def load_slices(dicom_dir: Path, enforce_dcm = False):
     files = []
@@ -14,7 +13,7 @@ def load_slices(dicom_dir: Path, enforce_dcm = False):
     print(f"file count: {len(files)}")
     return files
 
-def slices_to_np(files, plot=False):
+def slices_to_np(files) -> np.ndarray:
     # skip files with no SliceLocation (eg scout views)
     slices = []
     skipcount = 0
@@ -28,14 +27,7 @@ def slices_to_np(files, plot=False):
 
     # ensure they are in the correct order
     slices = sorted(slices, key=lambda s: s.SliceLocation)
-
-    # pixel aspects, assuming all slices are the same
-    ps = slices[0].PixelSpacing
-    ss = slices[0].SliceThickness
-    ax_aspect = ps[1] / ps[0]
-    sag_aspect = ps[1] / ss
-    cor_aspect = ss / ps[0]
-
+    
     # create 3D array
     img_shape = list(slices[0].pixel_array.shape)
     img_shape.append(len(slices))
@@ -45,21 +37,4 @@ def slices_to_np(files, plot=False):
     for i, s in enumerate(slices):
       img2d = s.pixel_array
       img3d[:, :, i] = img2d
-
-    if plot:
-        # plot 3 orthogonal slices
-        a1 = plt.subplot(2, 2, 1)
-        plt.imshow(img3d[:, :, img_shape[2] // 2])
-        a1.set_aspect(ax_aspect)
-
-        a2 = plt.subplot(2, 2, 2)
-        plt.imshow(img3d[:, img_shape[1] // 2, :])
-        a2.set_aspect(sag_aspect)
-
-        a3 = plt.subplot(2, 2, 3)
-        plt.imshow(img3d[img_shape[0] // 2, :, :].T)
-        a3.set_aspect(cor_aspect)
-
-        plt.show()
-
     return img3d
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import numpy as np
+import neurovolume as nv
 from pathlib import Path
 import dicom_to_numpy
 
 def main():
     # download your own sample from https://saga-it.com/dicom/samples
     dicom_series = Path("./ct-chest-lidc-idri-series")
-
-    slices = dicom_to_numpy.load_slices(dicom_series)
-    np = dicom_to_numpy.slices_to_np(slices, True)
+    dicom_slices = dicom_to_numpy.load_slices(dicom_series)
+    dicom_ndarray = dicom_to_numpy.slices_to_np(dicom_slices)
+    prepped_np = nv.prep_ndarray(dicom_ndarray)
+    # HINT: if srambled, try changing the cartesian_order
+    chest_grid = nv.Grid("chest", prepped_np)
+    # Note to self: it would be nice if .write() took the SaveConfig and had optional loudness
+    chest_volume = nv.Volume([chest_grid], save_config=nv.SaveConfig("chest", folder=Path("./output")))
+    chest_volume.write()
 
 if __name__ == "__main__":
     main()
